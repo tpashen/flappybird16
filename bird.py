7208c620a797d60455c878f23154761112bcbbda
tpashen <81863630+tpashen@users.noreply.github.com>
2021-06-05 16:05:51 +0300

    Create bird.py

---
+++ bird.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+pygame.init()
+
+widthDisplay = 500
+heightDisplay = 500
+ 
+back = (200, 255, 255)
+mw = pygame.display.set_mode((widthDisplay, heightDisplay))
+mw.fill(back)
+clock = pygame.time.Clock()
+ 
+game = True
